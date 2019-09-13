8970a9a7893acd900a2589529b27f8a2eb0a877c
yuvalgrossman <45980967+yuvalgrossman@users.noreply.github.com>
2019-09-13 10:08:45 +0300

    Update setup_VM.sh

--- setup_VM.sh
+++ setup_VM.sh
@@ -3,17 +3,29 @@
 #git clone https://github.com/yuvalgrossman/datahack_yyr
 
 sudo apt-get update
+# install python 3.7: 
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+sudo tar xzf Python-3.7.4.tgz
+cd Python-3.7.4
+sudo ./configure --enable-optimizations
+sudo make altinstall
+python3.7 -V
 
+cd ../../../home/grosman/
 # install pip: 
-sudo apt-get install python3-pip
-pip3 install --upgrade pip  
+sudo python3.7 -m pip install pip
+sudo python3.7 -m pip install --upgrade pip  
 
 #install virtualenv:
 sudo apt install virtualenv
 
 #create virtual environment:
 cd datahack_yyr
-virtualenv -p python3 venv
+virtualenv -p python3.7 venv
 source venv/bin/activate
 
 #install requirements
